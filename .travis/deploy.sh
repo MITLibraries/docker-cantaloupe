b381c950478b0daaa291878d88c0b503e65e2df8
Hardy Pottinger <hardy.pottinger@gmail.com>
2019-02-07 15:10:09 -0600

    refactor to work within the matrix, as per request from @ksclarke

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -12,12 +12,12 @@ echo '{"experimental": "enabled"}' > ~/.docker/config.json
 echo $DOCKER_PASSWORD | docker login --username $DOCKER_USERNAME --password-stdin
 
 # What tag are we building, latest stable or the latest dev branch?
-if [[ "$TRAVIS_BRANCH" == "$MASTER_BRANCH" && "$CANTALOUPE_VERSION" != "latest" ]]; then
+if [[ "$TRAVIS_BRANCH" == "$MASTER_BRANCH" && "$CANTALOUPE_VERSION" != "dev" ]]; then
   TAG=$(curl -S -H "Authorization: token $AUTH_TOKEN" \
     "https://api.github.com/repos/medusa-project/cantaloupe/releases/latest" \
     | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/' | cut -c2-)
 elif [[ "$TRAVIS_BRANCH" == "$MASTER_BRANCH" ]]; then
-  TAG="latest"
+  TAG="dev"
 fi
 
 # Make sure we found the latest stable if that's what we are building
@@ -29,7 +29,7 @@ else
     echo $?)
 
   # Right now, we're not deploying a tag that already exists in the registry
-  if [[ "$TAG_EXISTS" == "0" && "$TAG" != "latest" \
+  if [[ "$TAG_EXISTS" == "0" && "$TAG" != "dev" \
       && "$TRAVIS_BRANCH" == "$MASTER_BRANCH" && "$HARD_REGISTRY_PUSH" != "true" ]]; then
     echo "Release tag already exists; we don't need to push again"
   elif [[ "$TRAVIS_BRANCH" == "$MASTER_BRANCH" || "$HARD_REGISTRY_PUSH" == "true" ]]; then
@@ -43,4 +43,3 @@ else
     docker push "${REG_OWNER}/${REG_PROJECT}:${TAG}"
   fi
 fi
-
