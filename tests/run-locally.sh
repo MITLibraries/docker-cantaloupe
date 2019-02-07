4ceb92889e8edfc1a6dda73417394612f5b4f8f3
Hardy Pottinger <hardy.pottinger@gmail.com>
2019-02-07 16:17:31 -0600

    removed tests folder

--- tests/run-locally.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-# List of different types we want to run
-CANTALOUPE_VERSION="stable" COMMIT_REF="437a72d7" "$DIR/run-tests.sh"
-CANTALOUPE_VERSION="latest" COMMIT_REF="437a72d7" "$DIR/run-tests.sh"
