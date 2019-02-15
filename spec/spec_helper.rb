6c2c43953385e04644bdc4867e7373511d9d8c88
Hardy Pottinger <hardy.pottinger@gmail.com>
2019-02-15 12:54:37 -0600

    [IIIF-52] get tests to pass again while keeping rubocop happy

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,9 +14,11 @@ end
 
 def create_test_dockerfile(reg_username)
   template = File.read('spec/Dockerfile.erb')
+  cantaloupe_version = ENV['CANTALOUPE_VERSION']
   erb = ERB.new(template)
   dockerfile_content = erb.result(binding)
   tmp_dockerfile = Dir.mktmpdir + '/Dockerfile'
+  puts 'creating test dockerfile for cantaloupe version: ' + cantaloupe_version
   save_dockerfile(tmp_dockerfile, dockerfile_content)
   tmp_dockerfile
 end
